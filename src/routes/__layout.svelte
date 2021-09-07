dd2385706efcd33a09b86603e0210a1b2d930cd9
Igor Zanella <igorzanella.dev@gmail.com>
2021-09-07 02:26:08 +0200

    feat: add menu layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import 'carbon-components-svelte/css/g100.css';
+	import { Header, HeaderNav, HeaderNavItem } from 'carbon-components-svelte';
+</script>
+
+<Header platformName="RubberDuck" href="/">
+	<HeaderNav style="display: block !important">
+		<HeaderNavItem href="#what-is-rubber-duck" text="What is Rubber Duck?" />
+	</HeaderNav>
+</Header>
+
+<slot></slot>
